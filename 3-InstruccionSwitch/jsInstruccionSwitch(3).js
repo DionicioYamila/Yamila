cbc2fa1d3b9d78d41d7be5154fbd82e5b7eecd64
unknown <noelia_dionicio@hotmail.com>
2016-07-05 22:07:18 -0300

    hola

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,9 +1,29 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+var mesDelAño;
+mesDe1Año = document.getElementById('mes').value;
+switch (mesDe1Año)
+{
+	case "Febrero":
+	alert("Este mes no tiene mas de 29 dias.");
+	break;
+
+	case "Enero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	case "Julio":
+	case "Agosto":
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre"
+    case "Diciembre":
+    alert("Este mes tiene 30 o mas dias");
+    break;
+
+}
 	
 	
 
