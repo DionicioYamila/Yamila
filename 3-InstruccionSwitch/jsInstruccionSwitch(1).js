5c9d6f4925fe5f47b75ba7aa4818340a4f1e4745
unknown <noelia_dionicio@hotmail.com>
2016-07-05 21:02:31 -0300

    hola

---
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -0,0 +1,31 @@
+function Mostrar()
+{
+var mesDelAño; 
+ mesDelAño = document.getElementById('mes').value;
+ switch (mesDelAño)
+ {
+ case "Enero":
+ alert("que comiences bien el año!!!");
+ break;
+
+case "Marzo":
+alert("a clases!!!.");
+break;
+
+case "Julio":
+alert("se vienen las vacaciones");
+break;
+
+case "Diciembre":
+alert("felices fiestas");
+break;
+
+default:
+alert("");
+
+ }
+
+
+
+
+}//FIN DE LA FUNCIÓN
